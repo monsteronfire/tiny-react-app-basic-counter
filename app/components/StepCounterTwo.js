3ad0f12e32811b04d9b1967143f06039481872da
Shaii Ong <monsteronfire@gmail.com>
2017-06-30 23:04:09 +0800

    [WIP] Add parent/child component props

--- app/components/StepCounterTwo.js
+++ app/components/StepCounterTwo.js
@@ -3,11 +3,15 @@ import StepInput from './StepInput';
 import Counter from './Counter';
 
 class StepCounterTwo extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className='step-counter-two'>
         <Counter />
-        <StepInput />
+        <StepInput value={this.state.value} />
       </div>
     )
   }
--- app/components/StepInput.js
+++ app/components/StepInput.js
@@ -6,13 +6,25 @@ class StepInput extends React.Component {
     this.state = {
       value: 1
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('handleSubmit');
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value  });
   }
 
   render() {
     return (
-      <div className='step-input' onSubmit=''>
-        <form>
-          <input type='number' onChange='' />
+      <div className='step-input'>
+        <form onSubmit={this.handleSubmit}>
+          <input type='number' value={this.props.value} onChange={this.handleChange} />
           <input type='submit' value='Submit' />
         </form>
       </div>
