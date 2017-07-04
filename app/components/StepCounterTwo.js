7c320cc1fc1d5246d96785d90ddf7416542d76ad
Shaii Ong <monsteronfire@gmail.com>
2017-07-04 09:13:32 +0800

    Remove handleSubmit

--- app/components/StepCounterTwo.js
+++ app/components/StepCounterTwo.js
@@ -3,15 +3,25 @@ import StepInput from './StepInput';
 import Counter from './Counter';
 
 class StepCounterTwo extends React.Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      value: 1
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value  });
   }
 
   render() {
     return (
       <div className='step-counter-two'>
-        <Counter count={this.props.count} />
-        <StepInput value={this.props.value} />
+        <Counter count={this.state.count} />
+        <StepInput value={this.state.value} onChange={this.handleChange} />
       </div>
     )
   }
--- app/components/StepInput.js
+++ app/components/StepInput.js
@@ -1,31 +1,11 @@
 import React from 'react';
 
 class StepInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 1
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  handleChange(event) {
-    console.log('handleChange');
-    this.setState({value: event.target.value  });
-  }
-
   render() {
     return (
       <div className='step-input'>
-        <form onSubmit={this.handleSubmit}>
-          <input type='number' value={this.state.value} onChange={this.handleChange} />
-          <input type='submit' value='Submit' />
+        <form>
+          <input type='number' value={this.props.value} onChange={this.props.onChange} />
         </form>
       </div>
     )
